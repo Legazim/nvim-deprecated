0e24988192d9cd7a5b830ce9c6dbd5a255e27cd5
Héber Cruz <hccruz2001@outlook.com>
2022-10-10 09:49:51 -0300

    replacing files

--- init.lua
+++ init.lua
@@ -3,9 +3,10 @@ require('legazim.options')
 require('legazim.plugins')
 require('legazim.keybinds')
 require('legazim.colorscheme')
-
+-- Code completion and LSP
+require('legazim.cmp')
+require('legazim.lsp')
 -- Plugins configuration
-require('legazim.config.cmp')
 require('legazim.config.trim')
 require('legazim.config.lualine')
 require('legazim.config.mini')
@@ -13,4 +14,3 @@ require('legazim.config.nvimtree')
 require('legazim.config.telescope')
 require('legazim.config.toggleterm')
 require('legazim.config.scrollbar')
-require('legazim.lsp')
