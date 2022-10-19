2f15ce7fb786d4c8ec2f7fb558d6050a2b95b653
Héber Cruz <hccruz2001@outlook.com>
2022-10-19 09:32:20 -0300

    bufferline hover

--- lua/legazim/config/bufferline.lua
+++ lua/legazim/config/bufferline.lua
@@ -3,18 +3,6 @@ if not status_ok then
     return
 end
 
-vim.keymap.set('n', '<leader>bp', ':BufferLineTogglePin<cr>', { silent = true })
-vim.keymap.set('n', '<leader>b1', ':lua require("bufferline").go_to_buffer(1, true)<cr>', { silent = true })
-vim.keymap.set('n', '<leader>b2', ':lua require("bufferline").go_to_buffer(2, true)<cr>', { silent = true })
-vim.keymap.set('n', '<leader>b3', ':lua require("bufferline").go_to_buffer(3, true)<cr>', { silent = true })
-vim.keymap.set('n', '<leader>b4', ':lua require("bufferline").go_to_buffer(4, true)<cr>', { silent = true })
-vim.keymap.set('n', '<leader>b5', ':lua require("bufferline").go_to_buffer(5, true)<cr>', { silent = true })
-vim.keymap.set('n', '<leader>b6', ':lua require("bufferline").go_to_buffer(6, true)<cr>', { silent = true })
-vim.keymap.set('n', '<leader>b7', ':lua require("bufferline").go_to_buffer(7, true)<cr>', { silent = true })
-vim.keymap.set('n', '<leader>b8', ':lua require("bufferline").go_to_buffer(8, true)<cr>', { silent = true })
-vim.keymap.set('n', '<leader>b9', ':lua require("bufferline").go_to_buffer(9, true)<cr>', { silent = true })
-vim.keymap.set('n', '<leader>b-1', ':lua require("bufferline").go_to_buffer(-1, true)<cr>', { silent = true })
-
 bufferline.setup({
     options = {
         separator_style = 'slant',
@@ -37,10 +25,22 @@ bufferline.setup({
                 separator = true -- use a "true" to enable the default, or set your own character
             }
         },
+        hover = {
+            enabled = true,
+            delay = 200,
+            reveal = { 'close' }
+        }
     }
 })
 
-  -- * `raise` - a helper function to convert the passed number to superscript e.g. `raise(id)`.
-  -- * `lower` - a helper function to convert the passed number to subscript e.g. `lower(id)`.
-  -- * `ordinal` - The buffer ordinal number.
-  -- * `id` - The buffer ID.
+vim.keymap.set('n', '<leader>bp', ':BufferLineTogglePin<cr>', { silent = true })
+vim.keymap.set('n', '<leader>b1', ':lua require("bufferline").go_to_buffer(1, true)<cr>', { silent = true })
+vim.keymap.set('n', '<leader>b2', ':lua require("bufferline").go_to_buffer(2, true)<cr>', { silent = true })
+vim.keymap.set('n', '<leader>b3', ':lua require("bufferline").go_to_buffer(3, true)<cr>', { silent = true })
+vim.keymap.set('n', '<leader>b4', ':lua require("bufferline").go_to_buffer(4, true)<cr>', { silent = true })
+vim.keymap.set('n', '<leader>b5', ':lua require("bufferline").go_to_buffer(5, true)<cr>', { silent = true })
+vim.keymap.set('n', '<leader>b6', ':lua require("bufferline").go_to_buffer(6, true)<cr>', { silent = true })
+vim.keymap.set('n', '<leader>b7', ':lua require("bufferline").go_to_buffer(7, true)<cr>', { silent = true })
+vim.keymap.set('n', '<leader>b8', ':lua require("bufferline").go_to_buffer(8, true)<cr>', { silent = true })
+vim.keymap.set('n', '<leader>b9', ':lua require("bufferline").go_to_buffer(9, true)<cr>', { silent = true })
+vim.keymap.set('n', '<leader>b-1', ':lua require("bufferline").go_to_buffer(-1, true)<cr>', { silent = true })
