2d874d98b4b1fc812f78955f3c4baac30b61d777
Héber Cruz <hccruz2001@outlook.com>
2022-10-19 11:37:04 -0300

    harpoon keybinds

--- lua/legazim/config/harpoon.lua
+++ lua/legazim/config/harpoon.lua
@@ -1,18 +1,23 @@
-local status_ok, harpoon = pcall(require, "harpoon")
+local status_ok, harpoon = pcall(require, 'harpoon')
 if not status_ok then
-	return
+    return
 end
 
 local opts = { noremap = true }
-vim.keymap.set("n", ";m", ':lua require("harpoon.mark").toggle_file()<cr>', opts)
-vim.keymap.set("n", ";c", ':lua require("harpoon.mark").clear_all()<cr>')
-vim.keymap.set("n", ";;", ':lua require("harpoon.ui").toggle_quick_menu()<cr>', opts)
-vim.keymap.set("n", ";n", ':lua require("harpoon.ui").nav_next()<cr>', opts)
-vim.keymap.set("n", ";p", ':lua require("harpoon.ui").nav_prev()<cr>', opts)
-vim.keymap.set("n", ";1", ':lua require("harpoon.ui").nav_file(1)<cr>', opts)
-vim.keymap.set("n", ";2", ':lua require("harpoon.ui").nav_file(2)<cr>', opts)
-vim.keymap.set("n", ";3", ':lua require("harpoon.ui").nav_file(3)<cr>', opts)
-vim.keymap.set("n", ";4", ':lua require("harpoon.ui").nav_file(4)<cr>', opts)
-vim.keymap.set("n", ";t", ':lua require("harpoon.term").gotoTerminal(1)<cr>', opts)
+vim.keymap.set('n', ';m', ':lua require("harpoon.mark").toggle_file()<cr>', opts)
+vim.keymap.set('n', ';c', ':lua require("harpoon.mark").clear_all()<cr>')
+vim.keymap.set('n', ';;', ':lua require("harpoon.ui").toggle_quick_menu()<cr>', opts)
+vim.keymap.set('n', ';n', ':lua require("harpoon.ui").nav_next()<cr>', opts)
+vim.keymap.set('n', ';p', ':lua require("harpoon.ui").nav_prev()<cr>', opts)
+
+vim.keymap.set('n', ';1', ':lua require("harpoon.ui").nav_file(1)<cr>', opts)
+vim.keymap.set('n', ';2', ':lua require("harpoon.ui").nav_file(2)<cr>', opts)
+vim.keymap.set('n', ';3', ':lua require("harpoon.ui").nav_file(3)<cr>', opts)
+vim.keymap.set('n', ';4', ':lua require("harpoon.ui").nav_file(4)<cr>', opts)
+vim.keymap.set('n', ';5', ':lua require("harpoon.ui").nav_file(5)<cr>', opts)
+vim.keymap.set('n', ';6', ':lua require("harpoon.ui").nav_file(6)<cr>', opts)
+vim.keymap.set('n', ';7', ':lua require("harpoon.ui").nav_file(7)<cr>', opts)
+vim.keymap.set('n', ';8', ':lua require("harpoon.ui").nav_file(8)<cr>', opts)
+vim.keymap.set('n', ';9', ':lua require("harpoon.ui").nav_file(9)<cr>', opts)
 
 harpoon.setup()
--- lua/legazim/config/toggleterm.lua
+++ lua/legazim/config/toggleterm.lua
@@ -3,17 +3,7 @@ if not status_ok then
     return
 end
 
--- " set
--- autocmd TermEnter term://*toggleterm#*
---       \ tnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>
---
--- " By applying the mappings this way you can pass a count to your
--- " mapping to open a specific window.
--- " For example: 2<C-t> will open terminal 2
--- nnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>
--- inoremap <silent><c-t> <Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>
-vim.cmd [[autocmd TermEnter term://*toggleterm#*
-       \ tnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<Cr>]]
+vim.cmd [[autocmd TermEnter term://*toggleterm#* tnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<Cr>]]
 vim.keymap.set('n', '<C-t>', '<Cmd>exe v:count1 . "ToggleTerm"<Cr>', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-t>', '<esc><Cmd>exe v:count1 . "ToggleTerm"<Cr>', { noremap = true, silent = true })
 
@@ -49,13 +39,11 @@ toggleterm.setup {
 function _G.set_terminal_keymaps()
     local opts = { noremap = true }
     vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "kj", [[<C-\><C-n>]], opts)
 
-    vim.api.nvim_buf_set_keymap(0, "t", "<m-h>", [[<C-\><C-n><C-W>h]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<m-j>", [[<C-\><C-n><C-W>j]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<m-k>", [[<C-\><C-n><C-W>k]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<m-l>", [[<C-\><C-n><C-W>l]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
